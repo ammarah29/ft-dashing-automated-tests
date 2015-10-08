a9f2d6fd7aa141754aa34923e8dd9cc12148c09b
Ahmad, Ammarah <ammarah.ahmad@ft.com>
2015-10-08 15:42:16 +0100

    8th Oct

---
+++ Dashing.feature
@@ -0,0 +1,15 @@
+Feature: Dashing Automation Tests
+
+  Scenario Outline: Test the response time for dashing
+    I make the request to dashing
+    I should see response <status_code>
+    Examples:
+    | endpoint  | status_code |
+    | Test      | 200         |
+
+  Scenario Outline: Test the hardcoded endpoint
+    I make a request to end point
+    I should see a list of host names if tiles are black
+    Examples:
+    | endpoint  | status_code |
+    | Test      | 200         |
---
+++ steps.py
@@ -0,0 +1,33 @@
+import requests
+from lettuce import *
+from nose.tools import assert_equal
+
+
+@step('I make the request to dashing')
+def load_dashing(step):
+    world.response = requests.get("http://dashing-dev.internal.ft.com/blue")
+
+
+@step('I should see response (\d+)')
+def check_status_code(step, status_code):
+    assert_equal(world.response.status_code, int(status_code))
+
+
+@step('I make a request to end point')
+def make_request(step):
+    world.response = requests.get('http://dashing-dev.internal.ft.com/widgets/lists/')
+
+
+@step('I should see a list of host names if tiles are black')
+def find_hosts(step):
+    array_hosts = world.response.text.split('\n')
+    length_array = len(array_hosts)
+    for i in range(0, length_array-1):
+        host_name = array_hosts[i].strip('_new.rb')
+        try:
+            world.get_response = requests.get('http://dashing-dev.internal.ft.com/widgets/'+host_name+'.json')
+            data = world.get_response.text
+        except:
+            print host_name
+
+
